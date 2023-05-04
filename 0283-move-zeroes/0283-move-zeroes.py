dc232129349af15ebe285e2a7bb08fc274530a99
gopinadh5h1 <gopinadh5h1@gmail.com>
2023-05-04 17:01:11 +0530

    Time: 144 ms (33.91%), Space: 14.5 MB (87.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,11 +7,8 @@ class Solution(object):
         n = len(nums); i=0;j=1
         while j < n:
             if nums[i] == 0 and nums[j] != 0:
-                temp = nums[i]
-                nums[i] = nums[j]
-                nums[j] = temp
-                i += 1
-                j += 1
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1 ; j += 1
             elif nums[i] == 0 and nums[j] == 0:
                 j += 1
             else:
